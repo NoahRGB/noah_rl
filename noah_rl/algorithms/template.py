e267de18983afb5ecd95d65d4110416e846addb9
NoahRGB <ncox2004@gmail.com>
2026-09-24 22:53:36 +0100

    template update

--- noah_rl/algorithms/template.py
+++ noah_rl/algorithms/template.py
@@ -7,36 +7,29 @@
 
 import hydra
 from omegaconf import DictConfig
-import gymnasium as gym
 
 from noah_rl.utils.utils import seed
 from noah_rl.utils.gym import GymEnv
+from noah_rl.utils.logging import Logger
 
-def train(cfg: DictConfig):
+
+def train(cfg: DictConfig, hydra_dir: str):
 
     seed(cfg.seed)
-    env = GymEnv(cfg.env_name, cfg.num_envs, seed=cfg.seed, **cfg.env_kwargs)
+    env = GymEnv(cfg.env_name, cfg.num_envs, seed=cfg.seed, wrappers=cfg.wrappers, **cfg.env_kwargs)
+    logger = Logger(cfg.num_envs, cfg.title, hydra_dir, tensorboard=True)
 
     states, _ = env.reset()
     num_rollouts = cfg.timesteps // (cfg.rollout_len * cfg.num_envs)
     for rollout in range(num_rollouts):
-
-        for timestep in range(cfg.rollout_len):
-            action = env.action_space.sample()
-            sprimes, rewards, is_terms, is_truncs, info = env.step(action)
-
-            # TODO store transition
-
-            states = env.reset_finished_envs(sprimes, is_terms, is_truncs)
-
-
-
-
+        actions = env.action_space.sample() 
+        sprimes, rewards, is_terms, is_truncs, info = env.step(actions)
 
 
 @hydra.main(config_path="../../params", config_name="alg", version_base=None)
 def main(cfg: DictConfig):
-    train(cfg)
+    run_dir = hydra.core.hydra_config.HydraConfig.get().runtime.output_dir
+    train(cfg, run_dir)
 
 if __name__ == "__main__":
     main()
